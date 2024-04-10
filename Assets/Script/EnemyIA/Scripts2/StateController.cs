57b437c6ae4b9f1f8434709ccf89b3a80289ff43
Escudero22 <joan.escudero.7e6@itb.cat>
2024-04-10 19:02:11 +0200

    ScritableObjects

---
+++ Assets/Script/EnemyIA/Scripts2/StateController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateController : MonoBehaviour
+{
+    public GameObject target;
+    public int HP;
+
+    public ScriptableState2 currentState;
+    void Update()
+    {
+        //currentState.action.OnUpdate(this);
+    }
+    public void StateTransition(ScriptableState2 state)
+    {
+        if(currentState.scriptableStateTransitions.Contains(state))
+        {
+            currentState.action.OnFinishedState();
+            currentState = state;
+           // currentState.action.OnSetState(this);
+        }
+    }
+}
