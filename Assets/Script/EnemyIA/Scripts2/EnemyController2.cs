7b198ee9cc73c8957edb4994eddaa39c0bdb56aa
Angel Navarrete Sanchez <angel.navarrete.7e6@itb.cat>
2024-04-16 19:03:35 +0200

    SO Enemy

--- Assets/Script/EnemyIA/Scripts2/EnemyController2.cs
+++ Assets/Script/EnemyIA/Scripts2/EnemyController2.cs
@@ -5,34 +5,13 @@ using UnityEngine;
 public class EnemyController2 : StateController
 {
     public float AttackDistance;
-    public float LifePoints;
-    public ScriptableState2 Patrol, Follow, Attack, Die, RunState;
-
-    public GameObject target = null;
-    private float distance = 0;
+    public float HP;
     private float nextHurt = 0;
 
-    public int HP;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        target = collision.gameObject;
-        CheckDistance();
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        StateTransition(Patrol);
-        target = null;
-    }
-    private void CheckDistance()
-    {
-        distance = (target.transform.position - transform.position).magnitude;
-        if (distance > AttackDistance)  StateTransition(Follow);
-        else  StateTransition(Attack);
-    }
     void Update()
     {
-        if (target != null)       CheckDistance();
+        StateTransition();
+        if(currentState.action!=null)   currentState.action.OnUpdate();
         if (Input.GetKey("space") && Time.time >= nextHurt)
         {
             OnHurt(1);
@@ -42,18 +21,15 @@ public class EnemyController2 : StateController
 
     public void OnHurt(float damage)
     {
-        GetComponent<AudioSource>().Play();
-        LifePoints -= damage;
-        if (LifePoints <= 0)    Death();
-        if (LifePoints <= 2)    Run();
+        HP -= damage;
     }
-    private void Run()
-    {
-        StateTransition(RunState);
-    }
-    private void Death()
-    {
-        StateTransition(Die);
-    } 
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        target = collision.gameObject;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        target = null;
+    }
 }
--- Assets/Script/EnemyIA/Scripts2/StateController.cs
+++ Assets/Script/EnemyIA/Scripts2/StateController.cs
@@ -2,23 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class StateController : MonoBehaviour
+public class StateController : MonoBehaviour
 {
-    public GameObject target;
-    public int HP;
-
-    public ScriptableState2 currentState;
-    void Update()
+    public NodeBehaviourTree currentState;
+    public GameObject target = null;
+    //protected NodeBehaviourTree stateToPlay = null;
+    public void StateTransition()
     {
-        //currentState.action.OnUpdate(this);
-    }
-    public void StateTransition(ScriptableState2 state)
-    {
-        if(currentState.scriptableStateTransitions.Contains(state))
+        if (!currentState.AbortCondition(this))
         {
-            currentState.action.OnFinishedState();
-            currentState = state;
-           // currentState.action.OnSetState(this);
+            if (currentState.Children.Count != 0)
+            {
+                bool cond = false;
+                int count = 0;
+                while (!cond && count != currentState.Children.Count)
+                {
+                    cond = CheckCondition(currentState.Children[count++]);
+                }
+                if (cond)
+                {
+                    if(currentState.action != null) currentState.action.OnFinishedState();
+                    currentState = currentState.Children[count - 1];
+                    if(currentState.action != null) currentState.action.OnSetState(this);
+                }
+            }
+        }
+        else
+            GoToRootState();
+    }
+    public void GoToRootState()
+    {
+        if (currentState.Parent != null)
+        {
+            if (currentState.action != null) currentState.action.OnFinishedState();
+            currentState = currentState.Parent;
+            GoToRootState();
         }
     }
+    public bool CheckCondition(NodeBehaviourTree node)
+    {
+        return node.Condition(this);
+        /*switch (node.type)
+        {
+            case TypeOfCondition.SIMPLE:
+                return node.Condition(this);
+            case TypeOfCondition.AND:
+                return node.AndCondition(node.Parent.cond, this);
+            case TypeOfCondition.OR:
+                return node.OrCondition(node.Parent.cond, this);
+            default:
+                return false;
+        }*/
+    }
 }
+/*
+    public enum TypeOfCondition
+{
+    SIMPLE,
+    AND,
+    OR
+}*/
